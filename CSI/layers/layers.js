a374931440dd55a01935c3b46851fa56882f6533
joseedo98 <145924542+joseedo98@users.noreply.github.com>
2024-10-08 17:32:12 -0600

    Add files via upload

--- CSI/layers/layers.js
+++ CSI/layers/layers.js
@@ -32,30 +32,30 @@ var lyr_Subestacin_1 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/Subestacin_1.png" /> Subestación'
             });
-var format_BajaEscalaCSI_2 = new ol.format.GeoJSON();
-var features_BajaEscalaCSI_2 = format_BajaEscalaCSI_2.readFeatures(json_BajaEscalaCSI_2, 
+var format_PequeaEscalaCSI_2 = new ol.format.GeoJSON();
+var features_PequeaEscalaCSI_2 = format_PequeaEscalaCSI_2.readFeatures(json_PequeaEscalaCSI_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:5367'});
-var jsonSource_BajaEscalaCSI_2 = new ol.source.Vector({
+var jsonSource_PequeaEscalaCSI_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_BajaEscalaCSI_2.addFeatures(features_BajaEscalaCSI_2);
-var lyr_BajaEscalaCSI_2 = new ol.layer.Vector({
+jsonSource_PequeaEscalaCSI_2.addFeatures(features_PequeaEscalaCSI_2);
+var lyr_PequeaEscalaCSI_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_BajaEscalaCSI_2, 
-                style: style_BajaEscalaCSI_2,
-                popuplayertitle: "Baja Escala CSI (%)",
+                source:jsonSource_PequeaEscalaCSI_2, 
+                style: style_PequeaEscalaCSI_2,
+                popuplayertitle: "Pequeña  Escala CSI (%)",
                 interactive: true,
-    title: 'Baja Escala CSI (%)<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_0.png" /> 0 - 20<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_1.png" /> 20 - 30<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_2.png" /> 30 - 40<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_3.png" /> 40 - 50<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_4.png" /> 50 - 60<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_5.png" /> 60 - 70<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_6.png" /> 70 - 80<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_7.png" /> 80 - 90<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_8.png" /> 90 - 100<br />\
-    <img src="styles/legend/BajaEscalaCSI_2_9.png" /> 100 - 100<br />'
+    title: 'Pequeña  Escala CSI (%)<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_0.png" /> 0 - 20<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_1.png" /> 20 - 30<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_2.png" /> 30 - 40<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_3.png" /> 40 - 50<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_4.png" /> 50 - 60<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_5.png" /> 60 - 70<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_6.png" /> 70 - 80<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_7.png" /> 80 - 90<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_8.png" /> 90 - 100<br />\
+    <img src="styles/legend/PequeaEscalaCSI_2_9.png" /> 100 - 100<br />'
         });
 var format_GranEscalaCSIkW_3 = new ol.format.GeoJSON();
 var features_GranEscalaCSIkW_3 = format_GranEscalaCSIkW_3.readFeatures(json_GranEscalaCSIkW_3, 
@@ -82,7 +82,7 @@ var lyr_GranEscalaCSIkW_3 = new ol.layer.Vector({
     <img src="styles/legend/GranEscalaCSIkW_3_8.png" /> 31000 - 31000<br />'
         });
 var group_JASEC = new ol.layer.Group({
-                                layers: [lyr_CostaRica_0,lyr_Subestacin_1,lyr_BajaEscalaCSI_2,lyr_GranEscalaCSIkW_3,],
+                                layers: [lyr_CostaRica_0,lyr_Subestacin_1,lyr_PequeaEscalaCSI_2,lyr_GranEscalaCSIkW_3,],
                                 fold: "open",
                                 title: "JASEC"});
 var group_GRANESCALA = new ol.layer.Group({
@@ -90,16 +90,16 @@ var group_GRANESCALA = new ol.layer.Group({
                                 fold: "open",
                                 title: "GRAN ESCALA"});
 
-lyr_CostaRica_0.setVisible(true);lyr_Subestacin_1.setVisible(true);lyr_BajaEscalaCSI_2.setVisible(true);lyr_GranEscalaCSIkW_3.setVisible(true);
+lyr_CostaRica_0.setVisible(true);lyr_Subestacin_1.setVisible(true);lyr_PequeaEscalaCSI_2.setVisible(true);lyr_GranEscalaCSIkW_3.setVisible(true);
 var layersList = [group_JASEC];
 lyr_Subestacin_1.set('fieldAliases', {'Tensión (': 'Tensión (kV)', 'X1': 'X1', 'Y1': 'Y1', 'Nombre': 'Nombre', });
-lyr_BajaEscalaCSI_2.set('fieldAliases', {'bus1': 'bus1', 'X1': 'X1', 'Y1': 'Y1', 'Potencia N': 'Potencia Nominal (kVA)', 'Capacidad': 'Capacidad Alojamiento DER (kW)', 'Circuito': 'Circuito', 'Capacida_1': 'Capacidad (%)', });
+lyr_PequeaEscalaCSI_2.set('fieldAliases', {'bus1': 'bus1', 'X1': 'X1', 'Y1': 'Y1', 'Potencia N': 'Potencia Nominal (kVA)', 'Capacidad': 'Capacidad Alojamiento DER (kW)', 'Circuito': 'Circuito', 'Capacida_1': 'Capacidad (%)', });
 lyr_GranEscalaCSIkW_3.set('fieldAliases', {'DSSNAME': 'DSSNAME', 'begin': 'begin', 'end': 'end', 'DER MT —': 'Capacidad Alojamiento DER (kW)', 'DER MT �_1': 'Circuito', });
 lyr_Subestacin_1.set('fieldImages', {'Tensión (': 'TextEdit', 'X1': 'TextEdit', 'Y1': 'TextEdit', 'Nombre': 'TextEdit', });
-lyr_BajaEscalaCSI_2.set('fieldImages', {'bus1': 'TextEdit', 'X1': 'TextEdit', 'Y1': 'TextEdit', 'Potencia N': 'TextEdit', 'Capacidad': 'TextEdit', 'Circuito': 'TextEdit', 'Capacida_1': 'TextEdit', });
+lyr_PequeaEscalaCSI_2.set('fieldImages', {'bus1': 'TextEdit', 'X1': 'TextEdit', 'Y1': 'TextEdit', 'Potencia N': 'TextEdit', 'Capacidad': 'TextEdit', 'Circuito': 'TextEdit', 'Capacida_1': 'TextEdit', });
 lyr_GranEscalaCSIkW_3.set('fieldImages', {'DSSNAME': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'DER MT —': 'TextEdit', 'DER MT �_1': 'TextEdit', });
 lyr_Subestacin_1.set('fieldLabels', {'Tensión (': 'inline label - always visible', 'X1': 'hidden field', 'Y1': 'inline label - visible with data', 'Nombre': 'inline label - always visible', });
-lyr_BajaEscalaCSI_2.set('fieldLabels', {'bus1': 'hidden field', 'X1': 'hidden field', 'Y1': 'hidden field', 'Potencia N': 'inline label - always visible', 'Capacidad': 'inline label - always visible', 'Circuito': 'inline label - always visible', 'Capacida_1': 'hidden field', });
+lyr_PequeaEscalaCSI_2.set('fieldLabels', {'bus1': 'hidden field', 'X1': 'hidden field', 'Y1': 'hidden field', 'Potencia N': 'inline label - always visible', 'Capacidad': 'inline label - always visible', 'Circuito': 'inline label - always visible', 'Capacida_1': 'hidden field', });
 lyr_GranEscalaCSIkW_3.set('fieldLabels', {'DSSNAME': 'hidden field', 'begin': 'hidden field', 'end': 'hidden field', 'DER MT —': 'inline label - always visible', 'DER MT �_1': 'inline label - always visible', });
 lyr_GranEscalaCSIkW_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
